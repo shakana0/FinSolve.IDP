0370fd84b883204ca17817548e219979403b80f0
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:40:04 +0100

    feat: add ProcessingResult

---
+++ FinSolve.IDP.Domain/Entities/ProcessingResult.cs
@@ -0,0 +1,18 @@
+﻿using FinSolve.IDP.Domain.ValueObjects;
+namespace FinSolve.IDP.Domain.Entities
+{
+    public class ProcessingResult
+    {
+        public DocumentId DocumentId { get; }
+        public string Summary { get; }
+        public Dictionary<string, object> MappedData { get; }
+
+        public ProcessingResult(DocumentId documentId, string summary, Dictionary<string, object> mappedData)
+        {
+            DocumentId = documentId;
+            Summary = summary;
+            MappedData = mappedData ?? new();
+        }
+    }
+
+}
