e99bc3bce111cf17f4e01b4fe6b90dfb0b868a3b
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:39:46 +0100

    feat: add Document

---
+++ FinSolve.IDP.Domain/Entities/Document.cs
@@ -0,0 +1,26 @@
+﻿using FinSolve.IDP.Domain.Enums;
+using FinSolve.IDP.Domain.ValueObjects;
+
+namespace FinSolve.IDP.Domain.Entities
+{
+    public class Document
+    {
+        public DocumentId Id { get; }
+        public string FileName { get; }
+        public string BlobPath { get; }
+        public DocumentType Type { get; }
+        public Hash Hash { get; } //idempotency
+        public Metadata Metadata { get; }
+
+        public Document(DocumentId id, string fileName, string blobPath, DocumentType type, Hash hash, Metadata metadata)
+        {
+            Id = id;
+            FileName = fileName;
+            BlobPath = blobPath;
+            Type = type;
+            Hash = hash;
+            Metadata = metadata;
+        }
+    }
+
+}
