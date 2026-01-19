9bb69f4b2b5ff7628ee338445111f31cc50dfa2e
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:54:54 +0100

    feat: add IMetadataExtractor

---
+++ FinSolve.IDP.Domain/Interfaces/IMetadataExtractor.cs
@@ -0,0 +1,9 @@
+﻿using FinSolve.IDP.Domain.Entities;
+
+namespace FinSolve.IDP.Domain.Interfaces
+{
+    public interface IMetadataExtractor
+    {
+        Task<Metadata> ExtractAsync(string content);
+    }
+}
