930da31c69841e61083d84bb57a557dafd65e56b
shakana0 <shakanajamal3@gmail.com>
2026-01-19 13:27:33 +0100

    feat: add UnsupportedFormatException

---
+++ FinSolve.IDP.Domain/Exceptions/UnsupportedFormatException.cs
@@ -0,0 +1,8 @@
+﻿namespace FinSolve.IDP.Domain.Exceptions
+{
+    public class UnsupportedFormatException : Exception
+    {
+        public UnsupportedFormatException(string format)
+            : base($"Unsupported document format: {format}") { }
+    }
+}
