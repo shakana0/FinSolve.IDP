e62178e4266604e052af204403064b8c2e22149b
shakana0 <shakanajamal3@gmail.com>
2026-01-19 13:13:08 +0100

    feat: add TxtDocumentReader

---
+++ FinSolve.IDP.Domain/Readers/TxtDocumentReader.cs
@@ -0,0 +1,14 @@
+﻿using FinSolve.IDP.Domain.Interfaces;
+using System.Text;
+
+namespace FinSolve.IDP.Domain.Readers
+{
+    public class TxtDocumentReader : IDocumentReader
+    {
+        public async Task<string> ReadAsTextAsync(Stream stream)
+        {
+            using var reader = new StreamReader(stream, Encoding.UTF8, leaveOpen: true);
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
