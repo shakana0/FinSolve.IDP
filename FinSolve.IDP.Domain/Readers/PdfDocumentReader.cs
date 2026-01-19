78c42f09dfaa0afc1cb9eae29ac2e79986da261b
shakana0 <shakanajamal3@gmail.com>
2026-01-19 13:13:40 +0100

    feat: add PdfDocumentReader

---
+++ FinSolve.IDP.Domain/Readers/PdfDocumentReader.cs
@@ -0,0 +1,13 @@
+﻿using FinSolve.IDP.Domain.Interfaces;
+
+namespace FinSolve.IDP.Domain.Readers
+{
+    public class PdfDocumentReader : IDocumentReader
+    {
+        public Task<string> ReadAsTextAsync(Stream stream)
+        {
+            throw new NotImplementedException(
+                "PDF reading requires an external library and should be implemented in Infrastructure.");
+        }
+    }
+}
