3733bc9d1c1d462bf20920d72e914cdfcaae3d46
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:38:42 +0100

    feat: add ValidationResult

---
+++ FinSolve.IDP.Domain/ValueObjects/ValidationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace FinSolve.IDP.Domain.ValueObjects
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; }
+        public List<string> Errors { get; }
+
+        public ValidationResult(bool isValid, List<string> errors)
+        {
+            IsValid = isValid;
+            Errors = errors ?? new List<string>();
+        }
+
+        public static ValidationResult Success() => new(true, []);
+        public static ValidationResult Failure(params string[] errors) => new(false, errors.ToList());
+    }
+
+}
