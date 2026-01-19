f009b36826a4c6a7c5998f978e22f9d847995f80
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:55:19 +0100

    feat: add IProcessingRules

---
+++ FinSolve.IDP.Domain/Interfaces/IProcessingRules.cs
@@ -0,0 +1,9 @@
+﻿using FinSolve.IDP.Domain.Entities;
+
+namespace FinSolve.IDP.Domain.Interfaces
+{
+    public interface IProcessingRules
+    {
+        Task<ProcessingResult> ApplyAsync(Document document, string content);
+    }
+}
