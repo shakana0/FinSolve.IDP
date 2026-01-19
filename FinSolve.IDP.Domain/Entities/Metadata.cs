72d5e78a1f5130735bbc162773528b3f781438a7
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:39:32 +0100

    feat: add Metadata

---
+++ FinSolve.IDP.Domain/Entities/Metadata.cs
@@ -0,0 +1,19 @@
+﻿namespace FinSolve.IDP.Domain.Entities
+{
+    public class Metadata
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public DateTime? CreatedDate { get; }
+        public Dictionary<string, string> CustomFields { get; }
+
+        public Metadata(string title, string author, DateTime? createdDate, Dictionary<string, string> customFields)
+        {
+            Title = title;
+            Author = author;
+            CreatedDate = createdDate;
+            CustomFields = customFields ?? new();
+        }
+    }
+
+}
