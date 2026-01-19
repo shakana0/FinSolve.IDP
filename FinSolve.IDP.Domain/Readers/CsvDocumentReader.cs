5a47e2a09d6d5be3c39dd09518f78c8cc75d620b
shakana0 <shakanajamal3@gmail.com>
2026-01-19 13:11:15 +0100

    feat: add CsvDocumentReader

---
+++ FinSolve.IDP.Domain/Readers/CsvDocumentReader.cs
@@ -0,0 +1,15 @@
+﻿using FinSolve.IDP.Domain.Interfaces;
+using System.Text;
+
+namespace FinSolve.IDP.Domain.Readers
+{
+    public class CsvDocumentReader : IDocumentReader
+    {
+        public async Task<string> ReadAsTextAsync(Stream stream)
+        {
+            using var reader = new StreamReader(stream, Encoding.UTF8, leaveOpen: true);
+            var content = await reader.ReadToEndAsync();
+            return content.Replace("\r\n", "\n");
+        }
+    }
+}
