121ab765e513f9eaeb649347ee13da0adf1a60ef
shakana0 <shakanajamal3@gmail.com>
2026-01-19 13:14:18 +0100

    feat: add DocxDocumentReader

---
+++ FinSolve.IDP.Domain/Readers/DocxDocumentReader.cs
@@ -0,0 +1,13 @@
+﻿using FinSolve.IDP.Domain.Interfaces;
+
+namespace FinSolve.IDP.Domain.Readers
+{
+    public class DocxDocumentReader : IDocumentReader
+    {
+        public Task<string> ReadAsTextAsync(Stream stream)
+        {
+            throw new NotImplementedException(
+                "Docx reading requires an external library and should be implemented in Infrastructure.");
+        }
+    }
+}
