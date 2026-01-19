cbeed38872b6228f3c5ab28df73742747e701c75
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:37:52 +0100

    feat: add DocumentId

---
+++ FinSolve.IDP.Domain/ValueObjects/DocumentId.cs
@@ -0,0 +1,7 @@
+﻿namespace FinSolve.IDP.Domain.ValueObjects
+{
+    public readonly record struct DocumentId(Guid Value)
+    {
+        public static DocumentId New() => new(Guid.NewGuid());
+    }
+}
