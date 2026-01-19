eb3fc66c7a43e4aac4f20e28358c144dc54d54af
shakana0 <shakanajamal3@gmail.com>
2026-01-19 12:38:11 +0100

    feat: add Hash

---
+++ FinSolve.IDP.Domain/ValueObjects/Hash.cs
@@ -0,0 +1,7 @@
+﻿namespace FinSolve.IDP.Domain.ValueObjects
+{
+    public readonly record struct Hash(string Value)
+    {
+        public static Hash FromBytes(byte[] bytes) => new(Convert.ToHexString(bytes));
+    }
+}
