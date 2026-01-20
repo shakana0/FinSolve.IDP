2364857c519cb72f1d8504ce6c525064a0e6ff06
shakana0 <shakanajamal3@gmail.com>
2026-01-20 12:33:33 +0100

    feat(domain): introduce IPdfGenerator interface for PDF generation abstraction

---
+++ FinSolve.IDP.Domain/Interfaces/IPdfGenerator.cs
@@ -0,0 +1,9 @@
+﻿using FinSolve.IDP.Domain.Entities;
+
+namespace FinSolve.IDP.Domain.Interfaces
+{
+    public interface IPdfGenerator
+    {
+        Task<byte[]> GenerateAsync(Summary summary);
+    }
+}
