6801c8d30e97c2fb9517a5bfe99f551c3d8b58d5
shakana0 <shakanajamal3@gmail.com>
2026-01-20 12:39:18 +0100

    feat(application): add ProcessingMessageDto to represent incoming processing messages

---
+++ FinSolve.IDP.Application/DTOs/ProcessingMessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FinSolve.IDP.Application.DTOs
+{
+    public class ProcessingMessageDto
+    {
+        public Guid DocumentId { get; set; }
+        public string PrimaryCategory { get; set; } = string.Empty;
+        public List<string> Items { get; set; } = new();
+        public DateTime Timestamp { get; set; }
+    }
+}
