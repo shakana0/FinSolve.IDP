cec47590bff25745746cb4fa1a96bfc3a782ca6b
shakana0 <shakanajamal3@gmail.com>
2026-01-20 12:33:46 +0100

    feat(domain): add ISummaryGenerator to define summary generation contract

---
+++ FinSolve.IDP.Domain/Interfaces/ISummaryGenerator.cs
@@ -0,0 +1,9 @@
+﻿using FinSolve.IDP.Domain.Entities;
+
+namespace FinSolve.IDP.Domain.Interfaces
+{
+    public interface ISummaryGenerator
+    {
+        Task<Summary> GenerateAsync(ProcessingResult result);
+    }
+}
