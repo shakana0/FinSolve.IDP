5e0a04d87affe119cf82d9d1b8cef0fa41b7a02c
shakana0 <shakanajamal3@gmail.com>
2026-01-20 12:43:56 +0100

    feat(application): add DocumentReaderFactory to resolve appropriate reader based on DocumentType

---
+++ FinSolve.IDP.Application/Services/DocumentProcessing/DocumentReaderFactory.cs
@@ -0,0 +1,29 @@
+﻿using FinSolve.IDP.Domain.Enums;
+using FinSolve.IDP.Domain.Interfaces;
+using FinSolve.IDP.Domain.Readers;
+using FinSolve.IDP.Domain.Exceptions;
+
+namespace FinSolve.IDP.Application.Services.DocumentProcessing
+{
+    public class DocumentReaderFactory
+    {
+        private readonly IEnumerable<IDocumentReader> _readers;
+
+        public DocumentReaderFactory(IEnumerable<IDocumentReader> readers)
+        {
+            _readers = readers;
+        }
+
+        public IDocumentReader GetReader(DocumentType type)
+        {
+            return type switch
+            {
+                DocumentType.Txt => _readers.OfType<TxtDocumentReader>().First(),
+                DocumentType.Csv => _readers.OfType<CsvDocumentReader>().First(),
+                DocumentType.Pdf => _readers.OfType<PdfDocumentReader>().First(),
+                DocumentType.Docx => _readers.OfType<DocxDocumentReader>().First(),
+                _ => throw new UnsupportedFormatException(type.ToString())
+            };
+        }
+    }
+}
