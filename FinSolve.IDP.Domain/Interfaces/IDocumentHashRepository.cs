4f28ace56d4ed08dca97160a3a109bb91a902d2b
shakana0 <shakanajamal3@gmail.com>
2026-01-20 12:33:11 +0100

    feat(domain): add IDocumentHashRepository for idempotency hash persistence

---
+++ FinSolve.IDP.Domain/Interfaces/IDocumentHashRepository.cs
@@ -0,0 +1,10 @@
+﻿using FinSolve.IDP.Domain.ValueObjects;
+
+namespace FinSolve.IDP.Domain.Interfaces
+{
+    public interface IDocumentHashRepository
+    {
+        Task<bool> ExistsAsync(Hash hash);
+        Task SaveAsync(Hash hash);
+    }
+}
