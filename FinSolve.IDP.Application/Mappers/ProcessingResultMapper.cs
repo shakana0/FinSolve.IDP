9d5f2adc20e390c8d147d0210b14acf972fd4283
shakana0 <shakanajamal3@gmail.com>
2026-01-20 12:40:31 +0100

    feat(application): add ProcessingResultMapper to convert domain ProcessingResult into message DTO

---
+++ FinSolve.IDP.Application/Mappers/ProcessingResultMapper.cs
@@ -0,0 +1,19 @@
+﻿using FinSolve.IDP.Application.DTOs;
+using FinSolve.IDP.Domain.Entities;
+
+namespace FinSolve.IDP.Application.Mappers
+{
+    public static class ProcessingResultMapper
+    {
+        public static ProcessingMessageDto ToMessage(ProcessingResult result)
+        {
+            return new ProcessingMessageDto
+            {
+                DocumentId = result.DocumentId.Value,
+                PrimaryCategory = result.PrimaryCategory,
+                Items = result.Items.ToList(),
+                Timestamp = DateTime.UtcNow
+            };
+        }
+    }
+}
