9998a2f95b337c59d22925350f0c9f045152a694
shakana0 <shakanajamal3@gmail.com>
2026-01-21 15:19:54 +0100

    feat(application): add ILoggingAdapter for application-level logging abstraction

---
+++ FinSolve.IDP.Application/Interfaces/ILoggingAdapter.cs
@@ -0,0 +1,9 @@
+﻿namespace FinSolve.IDP.Application.Interfaces
+{
+    public interface ILoggingAdapter
+    {
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception? ex = null);
+    }
+}
