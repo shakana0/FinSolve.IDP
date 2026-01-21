9395c8ea6a123ee4fb230171fe478bc06b07dc9c
shakana0 <shakanajamal3@gmail.com>
2026-01-21 15:18:31 +0100

    feat(application): add IBlobStorage for abstracting blob operations

---
+++ FinSolve.IDP.Application/Interfaces/IBlobStorage.cs
@@ -0,0 +1,10 @@
+﻿namespace FinSolve.IDP.Application.Interfaces
+{
+    public interface IBlobStorage
+    {
+        Task<string> UploadAsync(
+            string blobName,
+            byte[] content,
+            IDictionary<string, string> metadata);
+    }
+}
