a4a6b0b2d7a9bf6c69e49d49cff61415e76b45b5
shakana0 <shakanajamal3@gmail.com>
2026-01-21 16:07:13 +0100

    feat(infrastructure): add ApplicationInsightsLogger for telemetry and logging

---
+++ FinSolve.IDP.Infrastructure/Telemetry/ApplicationInsightsLogger.cs
@@ -0,0 +1,37 @@
+﻿using FinSolve.IDP.Application.Interfaces;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace FinSolve.IDP.Infrastructure.Logging
+{
+    public class ApplicationInsightsLogger : ILoggingAdapter
+    {
+        private readonly TelemetryClient _telemetry;
+
+        public ApplicationInsightsLogger(TelemetryClient telemetry)
+        {
+            _telemetry = telemetry;
+        }
+
+        public void LogInformation(string message)
+        {
+            _telemetry.TrackTrace(message, SeverityLevel.Information);
+        }
+
+        public void LogWarning(string message)
+        {
+            _telemetry.TrackTrace(message, SeverityLevel.Warning);
+        }
+
+        public void LogError(string message, Exception? ex = null)
+        {
+            var trace = new TraceTelemetry(message, SeverityLevel.Error);
+            if (ex != null)
+            {
+                trace.Properties["Exception"] = ex.ToString();
+            }
+
+            _telemetry.TrackTrace(trace);
+        }
+    }
+}
