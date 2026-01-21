1aa74650894953271a630e1a786540b4f8441a67
shakana0 <shakanajamal3@gmail.com>
2026-01-21 15:17:54 +0100

    feat(application): add DocumentHashCosmosDto

---
+++ FinSolve.IDP.Application/DTOs/DocumentHashCosmosDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinSolve.IDP.Application.DTOs
+{
+    public class DocumentHashCosmosDto
+    {
+        public string id { get; set; } = default!;
+        public string DocumentId { get; set; } = default!;
+        public string Hash { get; set; } = default!;
+    }
+}
