c4599b85b98e1b5bcf678c509d9c1f7c9e11c04d
shakana0 <shakanajamal3@gmail.com>
2026-01-23 13:07:51 +0100

    feat(application): add interface for persisting dead-letter messages

---
+++ FinSolve.IDP.Application/Interfaces/IDlqRepository.cs
@@ -0,0 +1,10 @@
+﻿using FinSolve.IDP.Application.DTOs;
+
+namespace FinSolve.IDP.Application.Interfaces
+{
+    public interface IDlqRepository
+    {
+        Task SaveAsync(DeadLetterMessageDto message);
+    }
+
+}
