e6758ae6b26cda2b9f88a05388ed893442730a5d
shakana0 <shakanajamal3@gmail.com>
2026-02-19 10:48:03 +0100

    feat (domain): add property

--- FinSolve.IDP.Domain/Entities/Metadata.cs
+++ FinSolve.IDP.Domain/Entities/Metadata.cs
@@ -1,17 +1,21 @@
-﻿namespace FinSolve.IDP.Domain.Entities
+﻿using FinSolve.IDP.Domain.Enums;
+
+namespace FinSolve.IDP.Domain.Entities
 {
     public class Metadata
     {
         public string Title { get; }
         public string Author { get; }
         public DateTime? CreatedDate { get; }
+        public DocumentType? Type { get; }
         public Dictionary<string, string> CustomFields { get; }
 
-        public Metadata(string title, string author, DateTime? createdDate, Dictionary<string, string> customFields)
+        public Metadata(string title, string author, DateTime? createdDate, Dictionary<string, string> customFields, DocumentType type)
         {
             Title = title;
             Author = author;
             CreatedDate = createdDate;
+            Type = type;
             CustomFields = customFields ?? new();
         }
     }
