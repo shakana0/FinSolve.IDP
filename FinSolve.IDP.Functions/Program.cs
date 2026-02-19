acbb15eb9aaa00a21fdbce77a91e6067a07da95d
shakana0 <shakanajamal3@gmail.com>
2026-02-19 11:13:17 +0100

    feat (function): update program.cs by abstracting registration of dependencies

--- FinSolve.IDP.Functions/Program.cs
+++ FinSolve.IDP.Functions/Program.cs
@@ -1,16 +1,9 @@
-using Azure.Messaging.ServiceBus;
-using Azure.Storage.Blobs;
+using FinSolve.IDP.Application.Extensions;
 using FinSolve.IDP.Application.Interfaces;
-using FinSolve.IDP.Infrastructure.Blob;
-using FinSolve.IDP.Infrastructure.Cosmos;
-using FinSolve.IDP.Infrastructure.Dependencies.Cosmos;
+using FinSolve.IDP.Infrastructure.Extensions;
 using FinSolve.IDP.Infrastructure.KeyVault;
-using FinSolve.IDP.Infrastructure.Logging;
-using FinSolve.IDP.Infrastructure.Messaging;
-using FinSolve.IDP.Infrastructure.Pdf;
-using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Worker;
 
 
 var host = new HostBuilder()
@@ -24,60 +17,39 @@ var host = new HostBuilder()
     {
         var config = context.Configuration;
 
-        // Cosmos DB
-        services.AddSingleton<CosmosClient>(sp =>
-            new CosmosClient(config["Cosmos:ConnectionString"]));
-
-        services.AddSingleton<Container>(sp =>
-        {
-            var client = sp.GetRequiredService<CosmosClient>();
-            return client.GetContainer(config["Cosmos:Database"], config["Cosmos:Container"]);
-        });
-
-        services.AddSingleton<IProcessingResultRepository, ProcessingResultRepository>();
-        services.AddSingleton<IDocumentHashRepository, DocumentHashRepository>();
-
-        // Blob Storage
-        services.AddSingleton<BlobServiceClient>(sp =>
-            new BlobServiceClient(config["BlobStorage:ConnectionString"]));
-
-        services.AddSingleton<BlobContainerClient>(sp =>
-        {
-            var service = sp.GetRequiredService<BlobServiceClient>();
-            return service.GetBlobContainerClient(config["BlobStorage:ContainerName"]);
-        });
-
-        services.AddSingleton<IBlobStorage, AzureBlobStorage>();
-
-        // Service Bus
-        services.AddSingleton<ServiceBusClient>(sp =>
-            new ServiceBusClient(config["ServiceBus:ConnectionString"]));
-
-        services.AddSingleton<IMessagePublisher, ServiceBusMessagePublisher>();
-
-        // PDF
-        services.AddSingleton<IPdfGenerator, QuestPdfGenerator>();
+        services.AddApplicationServices();
+        services.AddInfrastructureServices(config);
 
         // Key Vault
         services.AddSingleton<IKeyVaultSecretProvider>(sp =>
         {
-            var keyVaultUrl = config["KeyVault:Url"];
+            var keyVaultUrl = config["KeyVaultUri"];
+
             if (string.IsNullOrEmpty(keyVaultUrl))
             {
-                throw new InvalidOperationException("KeyVault:Url configuration is missing");
+                throw new InvalidOperationException("Konfigurationsvärdet 'KeyVaultUri' saknas i appsettings eller miljövariabler.");
             }
+
             return new KeyVaultSecretProvider(keyVaultUrl);
         });
 
-        services.AddSingleton<IDocumentStatusRepository, DocumentStatusRepository>();
-        services.AddSingleton<IDlqRepository, DlqRepository>();
-
         // Application Insights
-        services.AddSingleton<TelemetryConfiguration>(sp =>
-            TelemetryConfiguration.CreateDefault());
+        services.AddApplicationInsightsTelemetryWorkerService(options =>
+        {
+            options.EnablePerformanceCounterCollectionModule = false;
+            options.EnableDependencyTrackingTelemetryModule = false;
+        });
 
-        services.AddSingleton<TelemetryClient>();
-        services.AddSingleton<ILoggingAdapter, ApplicationInsightsLogger>();
+        services.ConfigureFunctionsApplicationInsights();
+
+        // Sampling
+        services.Configure<TelemetryConfiguration>(config =>
+        {
+            var builder = config.DefaultTelemetrySink.TelemetryProcessorChainBuilder;
+            builder.UseSampling(5.0);
+            builder.Build();
+        });
+
     })
     .Build();
 
