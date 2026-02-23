e2ab11c9ead0930718da7dbdc89f627779134a1e
shakana0 <shakanajamal3@gmail.com>
2026-02-23 14:31:35 +0100

    refactor(domain): update IDocumentHashRepository to use Hash value object instead of string

--- FinSolve.IDP.Domain/Interfaces/IDocumentHashRepository.cs
+++ FinSolve.IDP.Domain/Interfaces/IDocumentHashRepository.cs
@@ -5,6 +5,6 @@ namespace FinSolve.IDP.Domain.Interfaces
     public interface IDocumentHashRepository
     {
         Task<bool> ExistsAsync(Hash hash);
-        Task SaveAsync(Hash hash);
+        Task SaveAsync(string documentId, Hash hash);
     }
 }
